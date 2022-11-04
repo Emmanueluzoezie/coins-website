e20fbe2b1066f123297af70417cdba6a591d543d
Aje Oluwasegun <ajeoluwasegun18@gmail.com>
2022-11-04 07:45:10 +0100

    added firebase config js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjvgaMHuNw5IVjsZWCaaL_miUbQ_bQydY",
+  authDomain: "coin-website-9028d.firebaseapp.com",
+  projectId: "coin-website-9028d",
+  storageBucket: "coin-website-9028d.appspot.com",
+  messagingSenderId: "925275113477",
+  appId: "1:925275113477:web:62a756ed993b4b9b2ab46c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
